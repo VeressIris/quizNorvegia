3a9e2e534f33a28b01d1d8eb14302f09c49270e5
VeressIris <irisveress@gmail.com>
2024-11-25 19:21:46 +0200

    added some more pages on the frontend

--- src/routes/quiz/[question]/+page.svelte
+++ src/routes/quiz/[question]/+page.svelte
@@ -24,10 +24,16 @@
 </script>
 
 <div class="flex flex-col items-center justify-center">
-	<h1 class="mb-8 text-2xl font-bold">{data.questionID}. Question title</h1>
-	<div class="grid grid-cols-2 grid-rows-2 gap-4">
+	<h1 class="text-2xl font-bold">{data.questionID}. Question title</h1>
+	<div class="my-8 grid grid-cols-2 grid-rows-2 gap-4">
 		{#each answers as answer, index}
 			<AnswerCard answer={answer.answer} isCorrect={answer.isCorrect} {index} />
 		{/each}
 	</div>
+	<button
+		class="rounded-xl bg-purple-500 px-6 py-2 text-xl font-bold text-white hover:bg-purple-600 active:bg-purple-800"
+		onclick={() => {
+			window.location = `/quiz/${Number(data.questionID) + 1}`;
+		}}>Next</button
+	>
 </div>
---
+++ src/routes/ready/+page.svelte
@@ -0,0 +1,8 @@
+<div class="flex flex-col items-center justify-center">
+	<h1 class="mb-3 text-3xl font-bold">Ești pregătit(ă)?</h1>
+	<p class="text-lg">Apasă butonul de mai jos pentru a începe quiz-ul.</p>
+	<a
+		class="mt-4 rounded-lg bg-green-600 px-4 py-2 text-xl font-bold text-white hover:bg-green-700 active:bg-green-800"
+		href="/quiz/1">ÎNCEPE</a
+	>
+</div>
