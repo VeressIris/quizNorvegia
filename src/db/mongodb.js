33b2683df47961d45a58b5391d0abc9262365a6b
VeressIris <irisveress@gmail.com>
2024-11-25 23:35:23 +0200

    idc ab hiding my uri anymore

--- src/db/mongodb.js
+++ src/db/mongodb.js
@@ -1,8 +1,6 @@
-import { MONGO_PASSWORD } from '$env/static/private';
-import { MONGO_USER } from '$env/static/private';
 import { MongoClient } from 'mongodb';
 
-const MONGO_URI = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@cluster0.rowjk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const MONGO_URI = `mongodb+srv://midnightdovedev:lJVPP2UeEsX2Etb1@cluster0.rowjk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(MONGO_URI);
 
